cba26cbed93e6bd30517744d0f53fa515a5a275a
Marla <Mar.mendez.alvez@gmail.com>
2024-08-08 21:25:12 -0300

    Suma

---
+++ soma.java
@@ -0,0 +1,10 @@
+public class soma {
+    public static void main(String[] args) {
+        int n1 = 2, n2 = 1;
+        int soma;
+
+        soma = n1 + n2;
+
+        System.out.println(soma);
+    }
+}
